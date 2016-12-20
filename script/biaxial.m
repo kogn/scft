3c018eee90c9d2bd0d938d744a911f2650123df2
kogn <kogn@gmail.com>
2016-12-20 16:46:40 +0800

    Change biaxial para

--- script/biaxial.m
+++ script/biaxial.m
@@ -3,6 +3,7 @@ function res = biaxial(output)
 res = zeros(m,1);
 tmp = add_eig(output);
 for i = 1:m
-    res(i) = 1-(sum(tmp(i,:).^2))^3/(sum(tmp(i,:).^3))^2/6;
+    %res(i) = 1-(sum(tmp(i,:).^2))^3/(sum(tmp(i,:).^3))^2/6;
+    res(i) = (sum(tmp(i,:).^2))^3-6*(sum(tmp(i,:).^3))^2;
 end
 
